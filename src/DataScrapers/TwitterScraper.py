991ea45ee253ec602d4224c0ce8b3350de64bfaf
arbrudi@gmail.com <arbrudi@gmail.com>
2024-12-21 17:35:17 +0100

    scraped more data and updated some code

--- src/DataScrapers/TwitterScraper.py
+++ src/DataScrapers/TwitterScraper.py
@@ -10,28 +10,18 @@ class TwitterScraper(BaseScraper):
         super().__init__(output_path)
         self.client = tweepy.Client(bearer_token=bearer_token, wait_on_rate_limit=True)
 
-    # TODO:
-    # Add a method that scrapes all tweets not just recent ones
-
-    def scrape_recent_tweets(self, query: str, count: int):
+    def scrape_tweets(self, query: str, count: int):
         """
         Method that scrapes and ingests recent tweets
 
         Args:
-            query (str): Represents the query which will be used to query the data
+            query (str): Represents the query which will be used to query the data.
             count (int): Represents the number of tweets we want to pull.
         """
         # TODO
-        # Check whether the csv file is open and cancel the run 
+        # Check whether the csv file is open and cancel the run
 
-        response = self.client.search_recent_tweets(
-                query=query,
-                tweet_fields=["author_id", "geo", "possibly_sensitive", "created_at", "public_metrics", "text", "source"],
-                user_fields=["username", "location"],
-                expansions="author_id", 
-                max_results=count
-            ) 
-        
+        response = self.get_recent_tweets(query, count)
         if response.data:
             attributes_cont = self.map_data(response)
 
@@ -43,7 +33,27 @@ class TwitterScraper(BaseScraper):
             self.logger.info(f"Successfully saved {len(x_df)} tweets to {self.output_path}")
         else:
             self.logger.info("No tweets found for this query!!!")
-        
+
+
+    def get_recent_tweets(self, query: str, count: int) -> tweepy.Response:
+        """
+        Retrieves recent tweets based on the specified query and count.
+
+        Args:
+            query (str): Represents the query which will be used to query the data.
+            count (int): Represents the number of tweets we want to pull.
+
+        Returns:
+            tweepy.Response: A response object that contains the tweets.
+        """
+        return self.client.search_recent_tweets(
+                    query=query,
+                    tweet_fields=["author_id", "geo", "possibly_sensitive", "created_at", "public_metrics", "text", "source"],
+                    user_fields=["username", "location"],
+                    expansions="author_id", 
+                    max_results=count
+                )
+
 
     def map_data(self, response: Any) -> list:
         """
